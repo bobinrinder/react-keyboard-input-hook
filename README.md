cd318d3b6ff40210b761d2b136d56eb109e8a9ed
bobinrinder <robin@m56studios.com>
2019-05-13 15:36:46 -0700

    added whitelist, blacklist and readme updates

--- README.md
+++ README.md
@@ -5,20 +5,29 @@ A [React Hook](https://reactjs.org/docs/hooks-intro.html) to easily work with ke
 
 `yarn add react-keyboard-input-hook`
 
-## Usage
+## Example Usage
 
 ```jsx
 import React from "react";
-import { useKeyUp } from "react-keyboard-input-hook";
+import { useKeyUp, useKeyDown, useKeyRepeat } from "react-keyboard-input-hook";
 
 function App() {
   // wrapping in useCallback is usually recommended
   const handleKeyUp = ({ keyName }) => {
-    console.log('the ' + keyName + ' was just pressed!');
+    console.log('the ' + keyName + ' was just released!');
+  };
+  const handleKeyDown = ({ keyName }) => {
+    console.log('the ' + keyName + ' was just pressed down!');
+  };
+  const handleKeyRepeat = ({ keyName }) => {
+    console.log('the ' + keyName + ' was just pressed down and held!');
   };
 
   // only destructure what you need, callback is optional
-  const { keyName, keyCode, keyCodeHistory } = useKeyUp(handleKeyUp);
+  const { keyCode, keyCodeHistory, keyName, keyNameHistory } = useKeyUp();
+  useKeyUp(handleKeyUp);
+  useKeyDown(handleKeyDown);
+  useKeyRepeat(handleKeyRepeat);
 
   return (
     <div>
@@ -31,6 +40,35 @@ function App() {
 
 export default App;
 ```
+
+## Reference
+
+### useKeyUp
+#### Parameters: 
+- **callback** (optional, default `null`):
+Callback function that gets executed on every event.
+- **whitelist** (optional, default `[]`):
+Whitelist of keyboard keyCodes (integer array) that are relevant, all other keys will be ignored.
+Note: Can only be used if `blacklist` paramater is an empty array.
+- **blacklist** (optional, default `[]`):
+Blacklist of keyboard keyCodes (integer array) that are irrelevant and therefore will be ignored.
+Note: Can only be used if `whitelist` paramater is an empty array.
+
+#### Return Object: 
+```jsx
+{ 
+  keyCode, // integer of last pressed keyCode
+  keyCodeHistory, // array of integers of recently pressed keyCodes
+  keyName, // string of last pressed key
+  keyNameHistory // array of strings of recently pressed keys
+}
+```
+
+### useKeyDown
+Same as `useKeyUp`
+
+### useKeyRepeat
+Same as `useKeyUp`
 
 ## License
 MIT
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
 import "./Buttons.js";
 
-export default function useKey(handleKeyCallback = null, type = "Up") {
+export default function useKey(
+  handleKeyCallback = null,
+  type = "Up",
+  whitelist = [],
+  blacklist = []
+) {
   // ensure valid event type
   if (type !== "Up" && type !== "Down" && type !== "Repeat") {
+    console.warn("useKey event type invalid, assumed type 'Up' instead");
     type = "Up";
+  }
+  // ensure only white- OR blacklist are set
+  if (whitelist.length > 0 && blacklist.length > 0) {
+    console.warn("White- and blacklist arrays > 0, emptied blacklist!");
+    blacklist = [];
   }
   // init state
   const [state, setState] = useState({
@@ -19,6 +30,14 @@ export default function useKey(handleKeyCallback = null, type = "Up") {
       // get key details from event
       const keyCode = e.detail.keyCode;
       const code = e.detail.code || "UnknownKey";
+
+      // check if white or blacklisted
+      if (whitelist.length > 0 && whitelist.indexOf(keyCode) === -1) {
+        return;
+      }
+      if (blacklist.length > 0 && blacklist.indexOf(keyCode) > -1) {
+        return;
+      }
 
       // update state with new key details
       setState(prevState => {
@@ -43,7 +62,7 @@ export default function useKey(handleKeyCallback = null, type = "Up") {
     window.addEventListener("key" + type, handleKey);
     // cleanup event listener
     return () => window.removeEventListener("key" + type, handleKey);
-  }, [handleKeyCallback, type]);
+  }, [handleKeyCallback, type, blacklist, whitelist]);
 
   return {
     keyCode: state.keyCode,
@@ -53,14 +72,26 @@ export default function useKey(handleKeyCallback = null, type = "Up") {
   };
 }
 
-export function useKeyUp(handleKeyUpCallback = null) {
-  return useKey(handleKeyUpCallback, "Up");
+export function useKeyUp(
+  handleKeyUpCallback = null,
+  whitelist = [],
+  blacklist = []
+) {
+  return useKey(handleKeyUpCallback, "Up", whitelist, blacklist);
 }
 
-export function useKeyDown(handleKeyDownCallback = null) {
-  return useKey(handleKeyDownCallback, "Down");
+export function useKeyDown(
+  handleKeyDownCallback = null,
+  whitelist = [],
+  blacklist = []
+) {
+  return useKey(handleKeyDownCallback, "Down", whitelist, blacklist);
 }
 
-export function useKeyRepeat(handleKeyRepeatCallback = null) {
-  return useKey(handleKeyRepeatCallback, "Repeat");
+export function useKeyRepeat(
+  handleKeyRepeatCallback = null,
+  whitelist = [],
+  blacklist = []
+) {
+  return useKey(handleKeyRepeatCallback, "Repeat", whitelist, blacklist);
 }
